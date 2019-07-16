ea8b8bfdd0af6857d37997e5ba758c151a004801
lschimm <lschimmelfennig@gmail.com>
2019-07-16 17:15:36 -0500

    woops. working on delete

--- cars/car-router.js
+++ cars/car-router.js
@@ -7,6 +7,7 @@ const db = knex(knexConfig.development);
 
 const router = express.Router();
 
+// get
 router.get("/", async (req, res) => {
   try {
     const cars = await db("cars");
@@ -16,7 +17,19 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.delete("/:id", async (req, res) => {});
+router.delete("/:id", async (req, res) => {
+  try {
+    const car = await db.getById("req.params.id");
+    if (car) {
+      await db.remove(req.params.id);
+      return res.status(200).json(car);
+    } else {
+      return res.status(404).json({ message: "id doesn't exist" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: "Failed to delete" });
+  }
+});
 
 // post
 
