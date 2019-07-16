22c1ec373357a4625f4cacff34ecbf5fcc962219
lschimm <lschimmelfennig@gmail.com>
2019-07-16 14:53:22 -0500

    added helmet, server.js fixed up, etc

--- api/server.js
+++ api/server.js
@@ -1,3 +1,17 @@
 // import express, etc
 // import the car router and express router, etc
 // set the endpoint for carRouter
+
+const express = require("express");
+const helmet = require("helmet");
+
+const carRouter = require("../cars/car-router.js");
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+server.use("/api/cars", carRouter);
+
+module.exports = server;
